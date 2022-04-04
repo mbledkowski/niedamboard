1357c3c350b4bead1a905d3115a152187863631a
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-04-04 20:49:15 +0200

    Update

--- app/src/components/github.vue
+++ app/src/components/github.vue
@@ -1,40 +1,70 @@
 <template>
-  <!--Title-->
-  <h2>λ github /profile</h2>
-  <!--Content-->
-  <!--Image-->
-  <img :src="data.avatar_url" alt="{{data.name}}'s GitHub profile picture">
-  <p>
-    {{ data.name }} <span class="accent">[</span> {{data.login}} <span class="accent">]</span>
-  </p>
-  <p>
-    site: <a class="accent" :href="data.html_url">github.com/{{data.login}}</a>
-  </p>
+  <article>
+    <!--Title-->
+    <header>
+      <h2>λ github /profile</h2>
+    </header>
+    <!--Content-->
+    <section>
+      <!--Image-->
+      <img
+        :src="dataArray[0].avatar_url"
+        alt="{{dataArray[0].name}}'s GitHub profile picture"
+        width="189"
+      />
+      <!--About-->
+      <h3>[ about ]</h3>
+      <p>
+        {{ dataArray[0].name }}
+        <span class="accent">@</span>
+        {{ dataArray[0].login }}
+        <br />
+        {{ dataArray[0].bio }}
+        <br />
+        {{ dataArray[0].company }}; {{ dataArray[0].location }}
+        <br />site:
+        <a :href="`https://${dataArray[0].blog}`">{{ dataArray[0].blog }}</a>; hireable
+        <span v-if="!dataArray[0].hireable" class="accent">n't</span>
+        <br />
+        {{ dataArray[0].followers }} followers; {{ dataArray[0].public_repos }} repos; {{ dataArray[0].public_gists }} gists
+        <br />last activity:
+        <span v-if="lastActivity === -1">?</span>
+        <span v-else>{{ Math.floor(lastActivity / 60) }} hours and {{ lastActivity % 60 }} minutes ago</span>
+      </p>
+    </section>
+    <!--Activity-->
+    <section>
+      <h3>[ activity ]</h3>
+      <div v-for="(event, index) in dataArray[1].slice(0, 4)" :key="index">
+        <p>{{ event.type.slice(0, event.type.length - 5) }} {{ event.repo.name }}</p>
+        <div v-if="event.payload.commits">
+          <p v-for="(commit, index) in event.payload.commits.slice(0, 2)" :key="commit.sha">
+            <a :href="commit.url" >{{ commit.sha.slice(0, 6) }}: "{{ commit.message }}"</a>
+            <span v-if="index === 1 && event.payload.commits.length !== 2"> and {{event.payload.commits.length-2}} more</span>
+          </p>
+        </div>
+      </div>
+    </section>
+    <!--Site-->
+    <footer>
+      <p>
+        site:
+        <a class="accent" :href="dataArray[0].html_url">github.com/{{ dataArray[0].login }}</a>
+      </p>
+    </footer>
+  </article>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from "vue"
 
+import dayjs from "dayjs"
+
 interface Props {
-  data: {
+  dataArray: [{
     login: string,
-    id: number,
-    node_id: string,
     avatar_url: string,
-    gravatar_id: string,
-    url: string,
     html_url: string,
-    followers_url: string,
-    following_url: string,
-    gists_url: string,
-    starred_url: string,
-    subscriptions_url: string,
-    organizations_url: string,
-    repos_url: string,
-    events_url: string,
-    received_events_url: string,
-    type: string,
-    site_admin: boolean,
     name: string,
     company: string,
     blog: string,
@@ -49,35 +79,33 @@ interface Props {
     following: number,
     created_at: string,
     updated_at: string,
-  }
+  }, {
+    type: string,
+    repo: {
+      name: string,
+    },
+    payload: {
+      commits?: {
+        sha: string,
+        message: string,
+        url: string,
+      }[],
+    },
+  }[]],
+  lastActivity: number,
 }
 
 export default defineComponent({
   name: "hspWhois",
   data(): Props {
     return {
-      data: {
+      dataArray: [{
         login: "mbledkowski",
-        id: 23295125,
-        node_id: "MDQ6VXNlcjIzMjk1MTI1",
-        avatar_url: "https://avatars.githubusercontent.com/u/23295125?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/mbledkowski",
+        avatar_url: "https://avatars.githubusercontent.com/u/23295125",
         html_url: "https://github.com/mbledkowski",
-        followers_url: "https://api.github.com/users/mbledkowski/followers",
-        following_url: "https://api.github.com/users/mbledkowski/following{/other_user}",
-        gists_url: "https://api.github.com/users/mbledkowski/gists{/gist_id}",
-        starred_url: "https://api.github.com/users/mbledkowski/starred{/owner}{/repo}",
-        subscriptions_url: "https://api.github.com/users/mbledkowski/subscriptions",
-        organizations_url: "https://api.github.com/users/mbledkowski/orgs",
-        repos_url: "https://api.github.com/users/mbledkowski/repos",
-        events_url: "https://api.github.com/users/mbledkowski/events{/privacy}",
-        received_events_url: "https://api.github.com/users/mbledkowski/received_events",
-        type: "User",
-        site_admin: false,
         name: "Maciej Błędkowski",
-        company: "Looking for a job (front-end)",
-        blog: "linkedin.com/in/mbledkowski",
+        company: "N/A",
+        blog: "mble.dk",
         location: "Gdańsk, Poland",
         email: null,
         hireable: true,
@@ -88,18 +116,31 @@ export default defineComponent({
         followers: 11,
         following: 120,
         created_at: "2016-11-06T15:05:52Z",
-        updated_at: "2021-12-23T22:25:55Z"
-      }
+        updated_at: "2021-12-23T22:25:55Z",
+      }, [{
+        type: "",
+        repo: {
+          name: "",
+        },
+        payload: {
+        },
+      }]],
+      lastActivity: -1,
     }
   },
   mounted() {
     this.fetchData()
   },
   methods: {
-    async fetchData() {
-      await fetch("https://api.github.com/user/23295125").then(res => res.json()).then(res => this.data = res)
-    }
-  }
+    fetchData() {
+      const profile = async () => {
+        await fetch("https://api.github.com/user/23295125").then(res => res.json()).then(res => this.dataArray[0] = res)
+        this.lastActivity = dayjs().diff(dayjs(this.dataArray[0].updated_at), 'minutes')
+      }
+      fetch("https://api.github.com/user/23295125/events").then(res => res.json()).then(res => this.dataArray[1] = res)
+      profile()
+    },
+  },
 })
 </script>
 
--- app/src/components/hspWhois.vue
+++ app/src/components/hspWhois.vue
@@ -1,43 +1,81 @@
 <template>
-  <!--Title-->
-  <h2>λ hsp.sh /whois</h2>
-  <!--Content-->
-  <p>{{ data.headcount }} people: <span class="accent">[</span>
-    <span v-for="(user, index) in data.users" :key="user">"{{user}}"<span v-if="index+1 !== data.users.length">, </span></span>
-    <span class="accent">]</span>
-  <br> and <a href="https://whois.at.hsp.sh/devices">{{data.unknown_devices}} unknown devices</a>
-  </p>
-  <p>site: <a class="accent" href="https://whois.at.hsp.sh/">whois.at.hsp.sh</a></p>
+  <article>
+    <!--Title-->
+    <header>
+      <h2>λ hsp.sh /whois</h2>
+    </header>
+    <!--Content-->
+    <section>
+      <div v-for="(data, index) in dataArray" :key="index">
+        <h3 v-if="index === 0">[ now ]</h3>
+        <h3 v-else>[ last 8 hours ]</h3>
+        <p>
+          <span v-if="data.headcount === -1">?</span>
+          <span v-else>{{ data.headcount }}</span> people:
+          <span class="accent">[</span>
+          <span v-for="(user, index) in data.users" :key="user">
+            "{{ user }}"
+            <span v-if="index + 1 !== data.users.length">,</span>
+          </span>
+          <span class="accent">]</span>
+          <br />and
+          <a v-if="index === 0" href="https://whois.at.hsp.sh/devices">
+            <span v-if="data.unknown_devices === -1">?</span>
+            <span v-else>{{ data.unknown_devices }}</span> unknown devices
+          </a>
+          <span v-else href="https://whois.at.hsp.sh/devices">
+            <span v-if="data.unknown_devices === -1">?</span>
+            <span v-else>{{ data.unknown_devices }}</span> unknown devices
+          </span>
+        </p>
+      </div>
+    </section>
+    <footer>
+      <p>
+        site:
+        <a class="accent" href="https://whois.at.hsp.sh/">whois.at.hsp.sh</a>
+      </p>
+    </footer>
+  </article>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from "vue"
 
 interface Props {
-  data: {
+  dataArray: {
     headcount: number,
     users: Array<string>,
     unknown_devices: number,
-  }
+  }[],
 }
 
 export default defineComponent({
   name: "hspWhois",
   data(): Props {
     return {
-      data: {
-        headcount: 0,
+      dataArray: [{
+        headcount: -1,
         users: [],
-        unknown_devices: 0,
-      }
+        unknown_devices: -1,
+      },
+      {
+        headcount: -1,
+        users: [],
+        unknown_devices: -1,
+      }]
     }
   },
   mounted() {
     this.fetchData()
+    setInterval(() => {
+      this.fetchData()
+    }, 12000)
   },
   methods: {
-    async fetchData() {
-      await fetch("https://whois.at.hsp.sh/api/now").then(res => res.json()).then(res => this.data = res)
+    fetchData() {
+      fetch("https://whois.at.hsp.sh/api/now").then(res => res.json()).then(res => this.dataArray[0] = res)
+      fetch("https://whois.at.hsp.sh/api/now?hours=8").then(res => res.json()).then(res => this.dataArray[1] = res)
     }
   }
 })
