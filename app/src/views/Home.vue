4c1561b3fedd20360ccf898380701a4c1f705c94
echo r"0xX4H" | rev <pub@mble.dk>
2023-05-12 19:26:47 +0200

    My story with HSP ends here

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -7,13 +7,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue"
-import hspWhois from "@/components/hspWhois.vue" // @ is an alias to /src
 import github from "@/components/github.vue"
 
 export default defineComponent({
   name: "Home",
   components: {
-    hspWhois,
     github,
   },
 })
